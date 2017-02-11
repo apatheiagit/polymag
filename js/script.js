5690d9416c0fba97b5aab46d732ee770eac84195
apatheiagit <apatheiar@mail.ru>
2017-02-11 15:16:49 +0300

    about4

--- js/script.js
+++ js/script.js
@@ -1,32 +1,51 @@
 $(function(){
-  /*sectionHeight();
-  $(window).resize(function(){
-    sectionHeight();
-  });*/
+
   $('.sandwitch').click(function(){
     $('.top-menu').toggleClass('top-menu--mobile-open');
   });
+
   $('.home-svg').addClass('animate');
+
   $('.section-home').addClass('intro-started');
+  
   $('.col-rectangle').click(function(){
     $('.solution-details').addClass('solution-details-open');
     var id = $(this).children('.rectangle').attr('data-detail');
     $('.solution-detail').hide();
     $('.solution-detail-'+id).show();
   });
+
   $('.close-buttn').click(function(){
     $('.solution-details').removeClass('solution-details-open');
   });
-  $('.people-small .item').click(function(){
-    $('.people-small .item').not(this).toggleClass('disappear');
-    $(this).toggleClass('active');
 
-  });  
+  $('.people-small .item .container-block').click(function(){
+    if($(this).parent().hasClass('active')){
+      $(this).parent().removeClass('active');
+      $('.people-small .item').removeClass('disappear');
+      //setTimeout(function(){ $('.people-small .item').removeClass('disappear');}, 100);
+    }else{
+      $('.people-small .item').addClass('disappear');
+      $(this).parent().removeClass('disappear').addClass('active');
+    }
+
+    //$('.people-small .item').not(this).toggleClass('disappear');
+    //$(this).toggleClass('active');
+  });
+
+  $('.nav-block').click(function(){
+    var next = $(this).attr('data-next');
+    $('.people-small .item').addClass('disappear');
+    $(this).parent().removeClass('active').addClass('disappear');
+    $('.item-'+next).removeClass('disappear').addClass('active');
+  }); 
+
   $(document).keyup(function(e) {
     if (e.keyCode == 27) { 
       $('.solution-details').removeClass('solution-details-open');
     }
   });
+
   $(window).scroll(function () {
       if ($(this).scrollTop() > 0) {
           $('.icon-to-top').fadeIn();
@@ -34,12 +53,14 @@ $(function(){
           $('.icon-to-top').fadeOut();
       }
   });
+
   $('.icon-to-top').click(function () {
       $('body,html').animate({
           scrollTop: 0
       }, 400);
       return false;
   });
+
   var paddingTop = '95px';
   if ($(window).height() < 901){
     paddingTop = '50px';
